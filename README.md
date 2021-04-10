00164de5e20bb5417628598a18ba1c63fed26985
chris <chris.mahfouz@hotmail.com>
2021-04-10 13:52:22 +0300

    added the counnter on likes

---
+++ README.md
@@ -0,0 +1,6 @@
+* Fetches all the meme from the server.
+* Select a meme randomly.
+* Page through the memes using and forward and back button.
+* You can like a meme .
+* If the same meme was liked multiple times the counter will update the number of likes.
+* You can remove the like message with a remove button.
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ let currentIndex = 0
 let totalMemes = 0
 
 document.addEventListener("DOMContentLoaded", () => {
+
     
-    form()
     getAllMemes()
-    
+
     document.getElementById('like').addEventListener('click', likeButton)
     document.getElementById('forward').addEventListener('click', next)
     document.getElementById('back').addEventListener('click', back)
-    document.getElementById('random').addEventListener('click',showRandom)
-   
-   
+    document.getElementById('random').addEventListener('click', showRandom)
+    document.getElementById('how-many-likes').addEventListener('click', removeLike)
+
 })
 
 
@@ -52,75 +52,63 @@ function drawImage() {
 
 function likeButton() {
     let ul = document.getElementById('how-many-likes')
-    let newLi = document.createElement('li')
+    let newLi
 
-    ul.appendChild(newLi)
+    let allLi = ul.getElementsByTagName('li')
 
-    newLi.innerText = `${allMemes[currentIndex].name} was liked!`
 
-    newLi.classList.add('like-text')
+
+    for (let i = 0; i < allLi.length; i++) {
+        if (parseInt(allLi[i].dataset.id) === currentIndex) {
+            newLi = allLi[i]
+        }
+
+    }
+
+    if (newLi === undefined) {
+        newLi = document.createElement('li')
+        newLi.dataset.id = currentIndex
+        newLi.dataset.count = 1
+
+        ul.appendChild(newLi)
+
+        newLi.innerText = `${allMemes[currentIndex].name} was liked ${newLi.dataset.count} times`
+        newLi.classList.add('like-text')
+    } else {
+        newLi.dataset.count++
+        newLi.innerText = `${allMemes[currentIndex].name} was liked ${newLi.dataset.count} times`
+    }
 
     let removeButton = document.createElement('button')
     removeButton.innerHTML = 'X'
 
     newLi.append(removeButton)
-    ul.addEventListener('click', removeLike)
-
+    
 }
+
 
 function removeLike(event) {
     console.log(event.target.innerText)
     if (event.target.innerText === "X") {
         event.target.parentElement.remove()
     }
+
+
 }
 
-    function next() {
+function next() {
 
-        if (currentIndex < totalMemes - 1) {
-            currentIndex++
-            drawImage()
-        }
+    if (currentIndex < totalMemes - 1) {
+        currentIndex++
+        drawImage()
     }
+}
 
-    function back() {
+function back() {
 
-        if (currentIndex > 0) {
-            currentIndex--
-            drawImage()
-        }
-    }
-
-function form() {
-
-    let newSearch = document.getElementById('search')
-    let newForm = document.createElement('form')
-    let newInput = document.createElement('input')
-    let submitInput = document.createElement('input')
-
-    newInput.placeholder = 'Search Meme'
-    newInput.value = ''
-    submitInput.type = "submit"
-    submitInput.value = 'SEARCH'
-
-    newSearch.appendChild(newForm)
-    newForm.appendChild(newInput)
-    newForm.appendChild(submitInput)
-
-    newForm.addEventListener('submit', search)
-
-
-    function search(event) {
-        event.preventDefault()
-        console.log(currentIndex)
-        console.log(totalMemes)
-
-        if (parseInt(newInput.value) <= totalMemes - 1) {
-            currentIndex = parseInt(newInput.value)
-            drawImage()
-        } else {
-            alert('you exceeded the total')
-        }
+    if (currentIndex > 0) {
+        currentIndex--
+        drawImage()
     }
 }
 
@@ -130,13 +118,13 @@ async function fetchMemes() {
     return data
 }
 
-
 function showRandom() {
-  
-      let random=Math.floor(Math.random() * totalMemes);
-      console.log(random)
-     currentIndex=random
-     
-     drawImage()
-     
+
+    let random = Math.floor(Math.random() * totalMemes);
+    console.log(random)
+    currentIndex = random
+
+    drawImage()
+
 }
+
